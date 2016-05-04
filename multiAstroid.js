dca913414873ff099eb34a80d5bc1478fee8ee34
Chuck Lan <chucklan@yahoo.com>
2016-05-04 16:58:03 -0700

    50 bots

--- multiAstroid.js
+++ multiAstroid.js
@@ -415,7 +415,7 @@ var t = setInterval(function() {
     var turretMoves = [];
     var rockMoves = [];
     var bulletMoves = [];
-    if (Object.keys(PlayerSession.all).length < 10) {
+    if (Object.keys(PlayerSession.all).length < 50) {
 	createBOT();
     }
     for (var key in PlayerSession.all) {
