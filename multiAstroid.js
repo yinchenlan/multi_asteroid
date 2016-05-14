aae1147a7908a9e55cce75502dacfc0fc83b525a
Chuck Lan <chucklan@yahoo.com>
2016-05-14 14:32:28 -0700

    Shorten guuid

--- multiAstroid.js
+++ multiAstroid.js
@@ -726,11 +726,12 @@ Rock.prototype = {
 
 function generateUUID(){
     var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var uuid = 'xxxxxxxx-xxxx-4xxx'.replace(/[xy]/g, function(c) {
 	    var r = (d + Math.random()*16)%16 | 0;
 	    d = Math.floor(d/16);
 	    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
 	});
+    //console.log(uuid);
     return uuid;
 }
 
